0194ae08260499ffef5971fcf7804a9f3ce6b308
root <tchenioguillaume@gmail.com>
2019-12-19 14:47:56 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Horloges </title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<?php
+		include '../header.php';
+	?>
+	<body>
+		<article class="clock">
+		  <div class="hours-container">
+		    <div class="hours"></div>
+		  </div>
+		  <div class="minutes-container">
+		    <div class="minutes"></div>
+		  </div>
+		  <div class="seconds-container">
+		    <div class="seconds"></div>
+		  </div>
+		</article>
+
+
+	</body>
+</html>
